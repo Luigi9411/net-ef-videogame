d39a805f6df414c786a99a83979f32fef808e124
Luigi9411 <luigi.sciavicco@gmail.com>
2023-04-03 01:32:06 +0200

    Aggiungere i file di progetto.

---
+++ net-ef-videogame/Software_house.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    [Table("Software_house")]
+    public class Software_house
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public long Tax_id { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public List<Software_house> Videogames { get; set; }
+    }
+}
---
+++ net-ef-videogame/Videogame.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace net_ef_videogame
+{
+    [Table("Videogame")]
+    [Index(nameof(VideogameId), IsUnique = true)]
+    public class Videogame
+    {
+        public long VideogameId { get; set; }
+        public string? Name { get; set; }
+        public string? Overview { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public long SoftwareHouseId { get; set; }
+        public Software_house Software_house { get; set; }
+    }
+}
